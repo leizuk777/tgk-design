ae19bbfc62cbc75fd6d3213324741dd4de00776d
leizuk777 <leizuk777@gmail.com>
2016-01-08 10:50:47 -0600

    First commit

---
+++ js/application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('#fullpage').fullpage({
+  sectionsColor: ['#009682', '#00B89F', '#00C2A8', '#00CCB1'],
+  anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+  });
+
+});
