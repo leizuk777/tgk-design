443b64f22531ef21e9906d0819da2d52c0cb740e
leizuk777 <leizuk777@gmail.com>
2016-01-09 17:31:36 -0600

    Add new js pages and upload new pictures'

--- js/application.js
+++ js/application.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     fixedElements: '.navbar_two',
     slidesNavigation: true,
     css3:true,
-    sectionsColor: ['#009682', '#00B89F', '#00C2A8', '#00CCB1'],
-    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+    sectionsColor: ['#009682', '#00B89F', '#00C2A8', '#00CCB1', '#00CCB1'],
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage']
   });
 
 });
