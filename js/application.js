152e40be36e9013549c41ecff1123986b5fcae9d
leizuk777 <leizuk777@gmail.com>
2016-01-11 12:04:47 -0600

    Add header transition and remove project slider

--- js/application.js
+++ js/application.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
+    $(document).on("scroll",function(){
+      if($(document).scrollTop()>100){
+        $("header").removeClass("large").addClass("small");
+      } else{
+        $("header").removeClass("small").addClass("large");
+      }
+    });
   $('#fullpage').fullpage({
     menu: '.navbar',
-    fixedElements: '.navbar_two',
     slidesNavigation: true,
     css3:true,
     sectionsColor: ['#009682', '#00B89F', '#00C2A8', '#00CCB1'],
-    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    afterRender: function(){},
+    fixedElements: 'header',
+    scrollBar:true
   });
-
 });
